841967f36adfe3a85c881b3d869d1b4bc8884af2
moonik <romanveos@gmail.com>
2018-09-21 13:22:04 +0200

    refactored heapsort

--- algorithms/heap_sort/heap_sort.py
+++ algorithms/heap_sort/heap_sort.py
@@ -1,33 +1,33 @@
 class HeapSort:
 
     def sort(self, values):
-        n = len(values)
+        n = len(values)-1
         i = n // 2
 
-        while i >= 1:
+        while i >= 0:
             self.__sink(values, i, n)
             i -= 1
 
-        while n > 1:
-            tmp = values[n-1]
-            values[n-1] = values[0]
+        while n >= 1:
+            tmp = values[n]
+            values[n] = values[0]
             values[0] = tmp
             n -= 1
-            self.__sink(values, 1, n)
+            self.__sink(values, 0, n)
 
     def __sink(self, values, k, n):
         while 2*k <= n:
             j = 2*k
 
-            if j < n and values[j-1] < values[j]:
+            if j < n and values[j] < values[j+1]:
                 j += 1
 
-            if not values[k-1] < values[j-1]:
+            if not values[k] < values[j]:
                 break
 
-            tmp = values[k-1]
-            values[k-1] = values[j-1]
-            values[j-1] = tmp
+            tmp = values[k]
+            values[k] = values[j]
+            values[j] = tmp
 
             k = j
 
